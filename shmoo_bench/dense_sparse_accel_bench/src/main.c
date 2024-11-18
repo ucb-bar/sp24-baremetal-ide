0812649260333cac170b7929404d13c46554398f
Ted Kim <tedkim7@bwrcrdsl-2.eecs.berkeley.edu>
2024-11-17 17:16:17 -0800

    Bring over a different test for sparse-sparse accelerator, test on hardware in lab

--- shmoo_bench/dense_sparse_accel_bench/src/main.c
+++ shmoo_bench/dense_sparse_accel_bench/src/main.c
@@ -31,6 +31,7 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #endif
 
+
 #define STR1(x) #x
 #ifndef STR
 #define STR(x) STR1(x)
@@ -38,6 +39,8 @@
 
 #define CAT_(A, B) A##B
 #define CAT(A, B) CAT_(A, B)
+
+// TODO: Define instructions properly based on our new inst
 
 #define ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, func7)                               \
   {                                                                                  \
@@ -55,16 +58,55 @@
         : "r"(rs1), "r"(rs2));                                                       \
   }
 
-void spMac(int8_t* A_ptr, int8_t A_stride, int8_t* B_ptr, int8_t B_stride, int8_t* C_ptr, int8_t C_stride, int8_t* CSR_ptr) {
+#define CACHELINE 64
+
+void MAC_model(int8_t A[16][16], int8_t B[16][16], int8_t C[16][16]) {
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            C[i][j] = 0;
+
+            for (int k = 0; k < 16; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void spMac(uint8_t* A_ptr, uint8_t A_stride, uint8_t* B_ptr, uint8_t B_stride, uint8_t* C_ptr, uint8_t C_stride, uint8_t* D_ptr, uint8_t D_stride, uint8_t* CSR_ptr) {
     ROCC_INSTRUCTION_0_R_R(0, A_ptr, A_stride , 0);
     ROCC_INSTRUCTION_0_R_R(0, B_ptr, B_stride , 1);
     ROCC_INSTRUCTION_0_R_R(0, C_ptr, C_stride , 2);
-    ROCC_INSTRUCTION_0_R_R(0, C_ptr, C_stride , 3); // C is the same as D for now
-    ROCC_INSTRUCTION_0_R_R(0, CSR_ptr, 1, 4);
+    ROCC_INSTRUCTION_0_R_R(0, D_ptr, D_stride , 3);
+    ROCC_INSTRUCTION_0_R_R(0, CSR_ptr, 0, 4);
     asm volatile ("fence");
 }
 
-#define CACHELINE 128
+// Equal probability of generating 0 or 1
+int spar50() {
+    return rand() & 1;
+}
+
+int spar25() {
+    return spar50() && spar50();
+}
+
+int spar12() {
+    return spar50() && spar50() && spar50();
+}
+
+int spar3() {
+    return spar12() && spar25();
+}
+
+int spar08() {
+    return spar3() && spar25();
+}
+
+static uint64_t read_cycles() {
+    uint64_t cycles;
+    asm volatile ("rdcycle %0" : "=r" (cycles));
+    return cycles;
+}
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -113,86 +155,109 @@ void app_main() {
 
 /* This test is a sanity test and simply tests multiplication of 2 values in the whole matrix*/
 
-    __attribute__((aligned(CACHELINE))) int8_t A[16][16] = {{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    __attribute__((aligned(CACHELINE))) uint8_t A[16][16];
+    uint8_t *a_ptr = A[0]; 
 
-    int8_t *a_ptr = A[0]; 
+    __attribute__((aligned(CACHELINE))) uint8_t B[16][16];
+    uint8_t *b_ptr = B[0]; 
 
-    // Use this to check for reading pattern
+    __attribute__((aligned(CACHELINE))) uint8_t C[16][16];
+    uint8_t *c_ptr = C[0];
 
-    __attribute__((aligned(CACHELINE))) int8_t B[16][16] = {{4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    __attribute__((aligned(CACHELINE))) uint8_t CSR[384] = {0};
+    uint8_t *csr_ptr = CSR;
+ 
+    __attribute__((aligned(CACHELINE))) uint8_t correct[16][16] = {0};
+    uint8_t *correct_ptr = correct[0];
 
-    int8_t *b_ptr = B[0]; 
+    // __attribute__((aligned(CACHELINE))) uint8_t D[16][16];
+    // uint8_t *d_ptr = D[0];
 
-    int8_t C[16][16];
-    
-    int8_t *c_ptr = C[0];
+    // printf("A: %p, B: %p, C: %p, CSR: %p, Correct: %p, D: %p \n", a_ptr, b_ptr, c_ptr, csr_ptr, correct_ptr, d_ptr);
 
-    int8_t CSR[16][16];
+    static int iterations = 5;
+    srand(time(NULL));
 
-    int8_t *csr_ptr = CSR[0];
+    for (int num=0; num < iterations; num++) {
 
-    int32_t mtime_start = clint_get_time(&CLINT);
-
-    spMac(a_ptr, 16, b_ptr, 16, c_ptr, 16, csr_ptr);
-
-    int32_t mtime_end = clint_get_time(&CLINT);
-
-    int32_t cycle_count = mtime_end - mtime_start;
-
-    printf("\nCycle count: %d", cycle_count); // 10
-
-    bool failed = false;
-    
-
-    for(int i=0; i<16; i++){
-        for (int j=0; j<16; j++) {
-            if (i == 0 && j == 0) {
-                if (C[i][j] != 4) {
-                    failed = true;
-                    printf("\nFAILED! Expected %d. Got %d.\n", 4, C[i][j]);
+        // Generate matrices
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                A[i][j] = (uint8_t)((rand()) % 255);
+                B[i][j] = (uint8_t)((rand()) % 255);
                 }
-            } else {
-                if (C[i][j] != 0) {
-                    failed = true;
-                    printf("\nFAILED! Expected %d. Got %d.\n", 0, C[i][j]);
+            }
+
+        //RoCC
+        uint64_t accel_start_cycles = read_cycles();
+        spMac(a_ptr, 16, b_ptr, 16, c_ptr, 16, NULL, 16, csr_ptr);
+        uint64_t accel_end_cycles = read_cycles();
+
+
+        //CPU
+        uint64_t cpu_start_cycles = read_cycles();
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                correct[i][j] = 0;
+                for (int k = 0; k < 16; k++) {
+                    correct[i][j] += (uint8_t)((uint8_t) A[i][k] * (uint8_t) B[k][j]);
                 }
+            }
+        }
+        uint64_t cpu_end_cycles = read_cycles();
+
+        // Checking Correctness
+        bool failed = false;
+
+        for (int i=0; i<16; i++) {
+            for (int j=0; j<16; j++) {
+                if (((uint8_t)C[i][j]) != ((uint8_t)(correct[i][j]))) {
+                    failed = true;
+                    // printf("Failed @(%d,%d), Expected:%3d, Got:%3d\n",i,j,correct[i][j],C[i][j]);
+                }
+            }
+        }
+
+        if (!failed) {
+            printf("================ Passed %d/%d================\n",num+1,iterations);
+            printf("ACCEL perf: %" PRIu64 "\n", accel_end_cycles-accel_start_cycles);
+            printf("CPU perf: %" PRIu64 "\n", cpu_end_cycles-cpu_start_cycles);
+
+        }else{
+            printf("================ Failed @ %d (%d/%d)================\n",num%10, num+1,iterations);
+            printf("A Matrix\n");
+            for (int i=0; i<16; i++) {
+                for (int j=0; j<16; j++) {
+                    printf("%6u", (A[i][j]));
+            }
+            printf("\n");
+            }
+            
+            printf("B Matrix\n");
+            for (int i=0; i<16; i++) {
+                for (int j=0; j<16; j++) {
+                    printf("%6u", (B[i][j]));
+            }
+            printf("\n");
+            }
+            printf("correct:\n");
+            for(int i=0; i<16; i++){
+                for (int j=0; j<16; j++) {
+                    printf("%6u", (correct[i][j]));
+            }
+            printf("\n");
+            }
+
+            printf("Output Matrix\n");
+            for (int i=0; i<16; i++) {
+                for (int j=0; j<16; j++) {
+                    printf("%6u", (C[i][j]));
+            }
+            printf("\n");
             }
         }
     }
 
-    if (!failed) {
-        printf("\nPASSED TEST!");
-    }
 }
 
 /* USER CODE END PUC */
