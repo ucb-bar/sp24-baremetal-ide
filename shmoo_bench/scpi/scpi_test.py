df6ed1d454438a25334a089f26f45e7e614c396b
Ted Kim <tedkim7@bwrcrdsl-2.eecs.berkeley.edu>
2024-11-20 00:46:02 -0800

    SCPI updates

--- shmoo_bench/scpi/scpi_test.py
+++ shmoo_bench/scpi/scpi_test.py
@@ -1,12 +1,45 @@
 ## A basic test of interactions with lab equipment via SCPI.
 
 import pyvisa
+import time
 
+# Establish connection to the power supply
 rm = pyvisa.ResourceManager()
 
-resources = rm.list_resources()
+psu = rm.open_resource('TCPIP0::192.168.1.100::INSTR')  # Replace this: Power Supply address
 
-# https://pyvisa.readthedocs.io/en/latest/introduction/communication.html#getting-the-instrument-configuration-right
+# Need to verify that this is the power supply. psu_verification should be 
+# something like "Keysight Technologies,E3646A,0,X.X-Y.Y-Z.Z"
+psu_verification = psu.query("*IDN?")
 
-my_instrument = rm.open_resource(resources[0])          # currently just using the first device that appears in the tuple
-my_instrument_type = my_instrument.query("*IDN?")       # to help identify what this instrument is
+# Reset both devices and configure initial settings
+psu.write("*RST")  # Reset the power supply
+
+# Set the current limit (for example, 2A), look into this and see if actually necessary
+psu.write("SOURce:CURRent 2") 
+
+# Define the range of voltages
+ 
+start_voltage = 0 # Starting voltage (in volts) 
+
+end_voltage = 5 # Ending voltage (in volts) 
+
+step_size = 0.5 # Step size (volts) 
+
+# Loop through the voltage range and set the voltage
+current_voltage = start_voltage 
+
+while current_voltage <= end_voltage: # Set the voltage   
+     psu.write(f"SOURce:VOLTage {current_voltage}")
+
+     # Query and print the current voltage setting 
+     voltage = psu.query("SOURce:VOLTage?") 
+     print(f"Voltage set to: {current_voltage} V") 
+ 
+     # Increment voltage by step size 
+     current_voltage += step_size
+
+     # Insert benchmark here to run at every step
+
+# Close the connections
+psu.close()
