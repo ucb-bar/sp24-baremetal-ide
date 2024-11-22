cc4509fd608cf5b5899ddb052990c0e69b24c23a
Ted Kim <tedkim7@bwrcrdsl-2.eecs.berkeley.edu>
2024-11-21 23:22:06 -0800

    Update scpi code to interact with power supply

--- shmoo_bench/scpi/scpi_test.py
+++ shmoo_bench/scpi/scpi_test.py
@@ -1,45 +1,48 @@
-## A basic test of interactions with lab equipment via SCPI.
+# A basic test of interactions with lab equipment via SCPI. Frequency will be adjusted via the PLL
 
 import pyvisa
 import time
 
 # Establish connection to the power supply
-rm = pyvisa.ResourceManager()
+rm = pyvisa.ResourceManager("@py")
 
-psu = rm.open_resource('TCPIP0::192.168.1.100::INSTR')  # Replace this: Power Supply address
+psu = rm.open_resource('TCPIP::169.254.58.10::gpib0,7::INSTR')
 
 # Need to verify that this is the power supply. psu_verification should be 
 # something like "Keysight Technologies,E3646A,0,X.X-Y.Y-Z.Z"
 psu_verification = psu.query("*IDN?")
 
-# Reset both devices and configure initial settings
 psu.write("*RST")  # Reset the power supply
 
 # Set the current limit (for example, 2A), look into this and see if actually necessary
 psu.write("SOURce:CURRent 2") 
-
-# Define the range of voltages
  
-start_voltage = 0 # Starting voltage (in volts) 
+start_voltage = 0.5
 
-end_voltage = 5 # Ending voltage (in volts) 
+end_voltage = 1.2
 
-step_size = 0.5 # Step size (volts) 
+step_size = 0.05  
 
 # Loop through the voltage range and set the voltage
 current_voltage = start_voltage 
 
-while current_voltage <= end_voltage: # Set the voltage   
-     psu.write(f"SOURce:VOLTage {current_voltage}")
+# Only channel 2 works
+psu.write("INST:SEL CH2")
 
-     # Query and print the current voltage setting 
-     voltage = psu.query("SOURce:VOLTage?") 
-     print(f"Voltage set to: {current_voltage} V") 
+# Set current to 100mA
+psu.write("CURR 0.1")
+
+while current_voltage <= end_voltage: # Set the voltage   
+
+     psu.write(f"VOLT {current_voltage}")
+
+     voltage = psu.query("VOLT?") 
+     print(f"Voltage set to: {voltage}V") 
  
-     # Increment voltage by step size 
      current_voltage += step_size
 
-     # Insert benchmark here to run at every step
+     # Insert benchmark here to run at every step.
+     # Write a benchmark that uses the PLL to adjust frequency, then have the Pico relay the frequency over to the PC.
+     # Or can think of another way to retrieve the PLL CLKOUT.
 
-# Close the connections
 psu.close()
